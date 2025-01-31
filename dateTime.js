ab79ee3f61cb9c0af6dbfc01a91c9b4031da1f5a
Shashvat-Rane <shashvatrane24@gmail.com>
2025-02-01 00:36:35 +0530

    Number, Math, Date & Time

---
+++ dateTime.js
@@ -0,0 +1,53 @@
+// Dates
+
+let mydate = new Date();
+
+// console.log(mydate);   // bakwas
+
+
+// console.log(mydate.toString());
+
+// console.log(mydate.toDateString());
+
+// console.log(mydate.toTimeString());
+
+// console.log(mydate.toLocaleString());
+
+// console.log(mydate.toLocaleDateString());
+
+// console.log(mydate.toLocaleTimeString());
+
+// 
+
+
+// Ways to create date
+
+// let myCreatedDate = new Date(2023, 0, 23);
+// let myCreatedDate = new Date(2023, 0, 23, 5, 3);
+// let myCreatedDate = new Date("2023-01-14");
+// let myCreatedDate = new Date("01-14-2023");
+
+// console.log(myCreatedDate.toLocaleString());
+
+
+let myTimeStamp = Date.now();
+
+// console.log(myTimeStamp);
+
+// console.log(myCreatedDate.getTime());  // in milliseconds
+
+// console.log(Math.floor(Date.now()/1000));  // in seconds
+
+
+
+let newDate = new Date();
+// console.log(newDate.toString());
+
+// console.log(mydate.getFullYear());
+// console.log(mydate.getMonth() + 1);
+// console.log(mydate.getDay());    //  1-> mon, 2->tue, 3->wed, 4->thu, 5->fri, 6->sat, 7->sunday
+// console.log(newDate.getDate());
+
+// console.log(newDate.getHours());
+// console.log(newDate.getMinutes());
+// console.log(newDate.getSeconds());
